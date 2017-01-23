292c9e65ecffaaa245fe7d6248c913ccdddbe8d0
superuser mintkde <mintkde@reyesaguayo.com>
2017-01-22 19:40:48 -0600

    adding comment.

--- R/dataProcess.R
+++ R/dataProcess.R
@@ -1,5 +1,7 @@
 #' Downloads a zip file, unpacks it on selected folder
 #'
+#' By default, if not indicated, the file will download in ./data.
+#'
 #' @param fileUrl the web link to the file
 #' @param outDir the folder where the file will be placed
 #' @return None
