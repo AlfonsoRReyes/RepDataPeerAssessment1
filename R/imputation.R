99d8537c04362dd6d4397c3c1f38bbc0087e73cd
superuser mintkde <mintkde@reyesaguayo.com>
2017-02-01 13:54:10 -0600

    trying to run mice's pmm from ImputeTestbench It didn't work out because ImputeTestbench wants to iterate the lenght of the vector times pmm.

--- R/imputation.R
+++ R/imputation.R
@@ -1,5 +1,10 @@
 # A sample function to randomly impute the missing data
 library(imputeTS)
+library(mice)
+library(RepDataPeerAssessment1)
+
+exit = FALSE
+out <- 0
 
 sss <- function(In){
   out <- na.random(In)
@@ -7,15 +12,23 @@ sss <- function(In){
   return(out)
 }
 
-pmm <- function(In) {
-  # this function will only work with dataset activity
-  library(mice)
-  library(RepDataPeerAssessment1)
 
+run.mice.pmm <- function() {
   df  <- activity[, c("steps", "interval")]
   res.mice <- mice(df, method = "pmm")
   mice.imp.1 <- mice::complete(res.mice, 1)
-  out <- mice.imp.1$steps
+  assign("exit", "TRUE", envir = .GlobalEnv)
+  assign("out", mice.imp.1$steps, envir = .GlobalEnv)
+  #return(mice.imp.1$steps)
+}
+
+pmm <- function(In) {
+  # this function will only work with dataset activity
+  if (exit == FALSE) {
+    run.mice.pmm()
+  }
   out <- as.numeric(out)
   return(out)
 }
+
+
