d4678205ffcb3c66765c5bb8763cf0e20d127445
superuser mintkde <mintkde@reyesaguayo.com>
2017-02-01 13:01:04 -0600

    add function pmm to use with ImputeTestbench

--- R/imputation.R
+++ R/imputation.R
@@ -6,3 +6,16 @@ sss <- function(In){
   out <- as.numeric(out)
   return(out)
 }
+
+pmm <- function(In) {
+  # this function will only work with dataset activity
+  library(mice)
+  library(RepDataPeerAssessment1)
+
+  df  <- activity[, c("steps", "interval")]
+  res.mice <- mice(df, method = "pmm")
+  mice.imp.1 <- mice::complete(res.mice, 1)
+  out <- mice.imp.1$steps
+  out <- as.numeric(out)
+  return(out)
+}
