62f91832d28ef75864a436c59bb36e4abc62dc76
superuser mintkde <mintkde@reyesaguayo.com>
2017-04-20 09:39:24 -0500

    disable global variables declaration

--- R/global.R
+++ R/global.R
@@ -1,9 +1,10 @@
-library(RepDataPeerAssessment1)
-library(rprojroot)
-
-project.data <- find_package_root_file('data')
-project.extdata <- find_package_root_file('inst/extdata')
-project.R <- find_package_root_file('R')
-project.data
-project.R
-project.extdata
+# library(RepDataPeerAssessment1)
+# library(rprojroot)
+#' globals
+# #' @importFrom rprojroot find_package_root_file
+# project.data <- find_package_root_file('data')
+# project.extdata <- find_package_root_file('inst/extdata')
+# project.R <- find_package_root_file('R')
+# project.data
+# project.R
+# project.extdata
